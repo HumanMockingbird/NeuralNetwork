f24113a7d3cbe69ba5dec810536ec44ace137ac0
sergey.beliaev <sergey.beliaev@indusoft.ru>
2026-06-02 16:15:03 +0300

    Added 2 methods to the Matrix class

--- Matrix.h
+++ Matrix.h
@@ -3,5 +3,29 @@ class Matrix
 {
 public:
 	double** values;
+	size_t rows, cols;
+
+	void MultiplyMatrixes(Matrix* A, Matrix* B)
+	{
+		if (CheckMultiplyAbility(A,B))
+		{
+			for (size_t i = 0; i < A->rows; i++)
+			{
+				for (size_t j = 0; j < B->cols; j++)
+				{
+					for (size_t k = 0; k < A->cols; k++)
+					{
+						this->values[i][j] += A->values[i][k] * B->values[k][j];
+					}
+				}
+			}
+		}
+		else
+	}
+
+	static bool CheckMultiplyAbility(Matrix* A, Matrix* B)
+	{
+		return (A->cols == B->rows);// Результат будет (A.rows x B.cols)
+	}
 };
 
--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -5,8 +5,5 @@ using namespace std;
 
 int main()
 {
-	Neuron n;
-	cin >> n.value;
-	n.activationFunction(Neuron::activationFunctionTypes::HyperbolicTangent, 1);
-	cout << n.actValue;
+	
 }
